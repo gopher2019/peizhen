ca2109cf204038c1ac7c6ada452020473835e458
chen <657499186@qq.com>
2025-06-17 21:53:11 +0800

    menu

---
+++ src/components/aside.vue
@@ -0,0 +1,41 @@
+<template>
+    <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        default-active="2"
+        class="aside-container"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+    >
+        <p class="logo-lg">Agent</p>
+
+        <TreeMenu />
+
+    </el-menu>
+</template>
+
+<script setup>
+import TreeMenu from "./treeMenu.vue";
+
+
+const handleOpen = () => {}
+const handleClose = () => {}
+
+
+
+</script>
+
+
+<style lang="less" scoped>
+.aside-container {
+    height: 100%;
+    .logo-lg {
+        font-size: 20px;
+        text-align: center;
+        height: 50px;
+        line-height: 50px;
+        color: #fff;
+    }
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-        layout
+    <div class="common-layout">
+        <el-container>
+            <Aside />
+
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
-<script setup>
 
+<script setup>
+import Aside from "../components/aside.vue";
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+    .common-layout {
+        height: 100%;
+        .el-container {
+            height: 100%;
+        }
+    }
 </style>
